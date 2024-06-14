5af0e9b901a554dcc8bb116927e94ed6b4b7ecb3
john <johnmaluki58@gmail.com>
2024-06-14 15:34:01 +0300

    first commit

---
+++ src/test/java/dev/johnmaluki/boardroom_booking_backend/BoardroomBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.johnmaluki.boardroom_booking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardroomBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
