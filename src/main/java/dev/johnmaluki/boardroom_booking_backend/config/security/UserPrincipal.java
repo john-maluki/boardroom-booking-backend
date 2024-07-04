4d00764b9d201a4dfcc50cb3548574b83aeb153e
john <johnmaluki58@gmail.com>
2024-07-04 20:19:35 +0300

    Enable date and time conversion based on user timezone

--- src/main/java/dev/johnmaluki/boardroom_booking_backend/config/security/UserPrincipal.java
+++ src/main/java/dev/johnmaluki/boardroom_booking_backend/config/security/UserPrincipal.java
@@ -50,4 +50,8 @@ public class UserPrincipal implements UserDetails {
         return this.user.getId();
     }
 
+    public String getUserTimeZone() {
+        return this.user.getTimeZone();
+    }
+
 }
--- src/main/java/dev/johnmaluki/boardroom_booking_backend/reservation/mapper/ReservationMapper.java
+++ src/main/java/dev/johnmaluki/boardroom_booking_backend/reservation/mapper/ReservationMapper.java
@@ -1,15 +1,22 @@
 package dev.johnmaluki.boardroom_booking_backend.reservation.mapper;
 
 
+import dev.johnmaluki.boardroom_booking_backend.config.security.UserPrincipal;
 import dev.johnmaluki.boardroom_booking_backend.reservation.dto.ReservationResponseDto;
 import dev.johnmaluki.boardroom_booking_backend.reservation.model.Reservation;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import java.time.*;
 import java.util.List;
 
 @Component
 public class ReservationMapper {
     public ReservationResponseDto toReservationResponseDto(Reservation reservation) {
+        String userTimeZone = this.getUserTimeZone();
+        ZonedDateTime zonedDateTimeStart = this.getZonedDateTime(reservation.getStartDate(), reservation.getStartTime(), userTimeZone);
+        ZonedDateTime zonedDateTimeEnd = this.getZonedDateTime(reservation.getEndDate(), reservation.getEndTime(), userTimeZone);
         return ReservationResponseDto.builder()
                 .id(reservation.getId())
                 .meetingTitle(reservation.getMeetingTitle())
@@ -17,10 +24,10 @@ public class ReservationMapper {
                 .meetingType(reservation.getMeetingType())
                 .approvalStatus(reservation.getApprovalStatus())
                 .attendees(reservation.getAttendees())
-                .startDate(reservation.getStartDate())
-                .endDate(reservation.getEndDate())
-                .startTime(reservation.getStartTime())
-                .endTime(reservation.getStartTime())
+                .startDate(zonedDateTimeStart.toLocalDate())
+                .endDate(zonedDateTimeEnd.toLocalDate())
+                .startTime(zonedDateTimeStart.toLocalTime())
+                .endTime(zonedDateTimeEnd.toLocalTime())
                 .boardroom(reservation.getBoardroom().getId())
                 .tag(reservation.getTag())
                 .build();
@@ -28,5 +35,19 @@ public class ReservationMapper {
 
     public List<ReservationResponseDto> toReservationResponseDtoList(List<Reservation> reservations){
         return reservations.stream().map(this::toReservationResponseDto).toList();
+    }
+
+    private ZonedDateTime getZonedDateTime(LocalDate date, LocalTime time, String userTimeZone) {
+        LocalDateTime dateTime = LocalDateTime.of(date, time);
+        ZoneId originalZoneId = ZoneId.of("UTC");
+        ZoneId targetZoneId = ZoneId.of(userTimeZone);
+        ZonedDateTime originalZonedDateTime = dateTime.atZone(originalZoneId);
+        return originalZonedDateTime.withZoneSameInstant(targetZoneId);
+    }
+
+    private String getUserTimeZone() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserPrincipal user = (UserPrincipal) auth.getPrincipal();
+        return user.getUserTimeZone();
     }
 }
