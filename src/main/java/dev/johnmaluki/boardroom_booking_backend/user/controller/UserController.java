64c769aadc051df560a78a31c506e5320fd22b4f
john <johnmaluki58@gmail.com>
2024-07-06 19:00:33 +0300

    Remove unnecessary imports

--- src/main/java/dev/johnmaluki/boardroom_booking_backend/user/controller/UserController.java
+++ src/main/java/dev/johnmaluki/boardroom_booking_backend/user/controller/UserController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
