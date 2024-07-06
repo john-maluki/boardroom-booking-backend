301f98285750dec20e96a844313f85cf191a082e
john <johnmaluki58@gmail.com>
2024-07-06 18:59:20 +0300

    Update getting users request method

--- src/main/java/dev/johnmaluki/boardroom_booking_backend/user/controller/UserController.java
+++ src/main/java/dev/johnmaluki/boardroom_booking_backend/user/controller/UserController.java
@@ -5,6 +5,7 @@ import dev.johnmaluki.boardroom_booking_backend.user.service.UserService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("/users")
+    @GetMapping("/users")
     public ResponseEntity<List<UserResponseDto>> getAllUsers() {
         return  ResponseEntity.ok(userService.getAllUsers());
     }
